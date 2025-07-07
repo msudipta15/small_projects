1964e43fdc479150f0aed89b8c92b9c58b2a5f90
Sudipta <techtv70@gmail.com>
2025-07-07 17:13:31 +0530

    .

--- fe1/src/App.tsx
+++ fe1/src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="h-screen w-full bg-gradient-to-b from-[#050a1e] px-16 via-[#07102f] to-[#09153f] ">
+    <div className="h-screen w-full bg-gradient-to-b from-[#050a1e] px-16 sm:px-56 via-[#07102f] to-[#09153f] ">
       <div className="w-full pt-10">
         <div className="w-full flex justify-between items-center bg-[#1f2535] py-3 px-2 rounded-2xl">
           <div>
@@ -19,6 +19,24 @@ function App() {
               />
             </button>
           </div>
+        </div>
+      </div>
+      <div className="w-full pt-10 px-2 flex flex-col justify-center items-center gap-3 sm:flex sm:flex-row  sm:justify-between">
+        <div>
+          <h1 className="text-3xl sm:text-4xl text-white font-bold">
+            Extensions List
+          </h1>
+        </div>
+        <div className="flex gap-2 text-lg">
+          <button className="w-fit h-fit p-2 bg-[#1f2535] px-4 text-white rounded-2xl cursor-pointer hover:bg-red-400 hover:text-black ">
+            All
+          </button>
+          <button className="w-fit h-fit p-2 bg-[#1f2535] px-4 text-white  rounded-2xl cursor-pointer hover:bg-red-400 hover:text-black">
+            Active
+          </button>
+          <button className="w-fit h-fit p-2 bg-[#1f2535] px-4 text-white  rounded-2xl cursor-pointer hover:bg-red-400 hover:text-black">
+            Inactive
+          </button>
         </div>
       </div>
     </div>
---
+++ fe1/src/components/card.tsx
@@ -0,0 +1,3 @@
+export function Card() {
+  return <div className="bg-[#1f2535] w-full  sm:w-[400px]">hello</div>;
+}
