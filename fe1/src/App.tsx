afce30422735df4c4c7131b423f85aaa53e8dac3
Sudipta <techtv70@gmail.com>
2025-07-07 16:05:12 +0530

    .

--- fe1/src/App.tsx
+++ fe1/src/App.tsx
@@ -1,5 +1,28 @@
 function App() {
-  return <div>hello</div>;
+  return (
+    <div className="h-screen w-full bg-gradient-to-b from-[#050a1e] px-16 via-[#07102f] to-[#09153f] ">
+      <div className="w-full pt-10">
+        <div className="w-full flex justify-between items-center bg-[#1f2535] py-3 px-2 rounded-2xl">
+          <div>
+            <img
+              src="src/assets/images/logo.svg"
+              alt="logo"
+              className="sm:w-[200px]"
+            />
+          </div>
+          <div>
+            <button className="w-fit h-fit p-2 hover:bg-[#52576a] rounded-2xl cursor-pointer">
+              <img
+                src="src/assets/images/icon-sun.svg"
+                alt=""
+                className="w-[25px] sm:w-[35px]"
+              />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
