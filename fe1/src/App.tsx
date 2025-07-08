a29eeac092f0cab74fcedae02bdbf3629fbd720b
Sudipta <techtv70@gmail.com>
2025-07-08 17:58:17 +0530

    .

--- fe1/src/App.tsx
+++ fe1/src/App.tsx
@@ -2,22 +2,26 @@ import { useEffect, useState } from "react";
 import { Card } from "./components/card";
 
 function App() {
-  const [darkmode, setdarkmode] = useState(true);
+  const [darkmode, setdarkmode] = useState(false);
 
   useEffect(() => {
     console.log(darkmode);
-
-    if (darkmode) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
   }, [darkmode]);
 
   return (
-    <div className="min-h-screen max-h-full w-full  pb-10 bg-gradient-to-b from-[#ecf3fd] via-[#edf6fb] to-[#effbfb] dark:bg-gradient-to-b dark:from-[#050a1e] px-10 sm:px-56 dark:via-[#07102f] dark:to-[#09153f]  ">
+    <div
+      className={`min-h-screen max-h-full w-full  pb-10  px-10 sm:px-56 ${
+        darkmode
+          ? "bg-gradient-to-b   dark:from-[#050a1e] dark:via-[#07102f] dark:to-[#09153f]"
+          : "bg-gradient-to-b from-[#ecf3fd] via-[#edf6fb] to-[#effbfb]"
+      }   `}
+    >
       <div className="w-full pt-10">
-        <div className="w-full flex justify-between items-center bg-[#1f2535] py-3 px-2 rounded-2xl">
+        <div
+          className={`w-full flex justify-between items-center ${
+            darkmode ? "bg-[#1f2535]" : "bg-[#fcfdff]"
+          }  py-3 px-2 rounded-2xl`}
+        >
           <div>
             <img
               src="src/assets/images/logo.svg"
@@ -28,31 +32,63 @@ function App() {
           <div>
             <button
               onClick={() => setdarkmode(!darkmode)}
-              className="w-fit h-fit p-2 hover:bg-[#52576a] rounded-2xl cursor-pointer"
+              className={`w-fit h-fit p-2 ${
+                darkmode ? "hover:bg-[#52576a]" : "hover:bg-slate-300"
+              }  rounded-2xl cursor-pointer`}
             >
-              <img
-                src="src/assets/images/icon-sun.svg"
-                alt=""
-                className="w-[25px] sm:w-[35px]"
-              />
+              {darkmode ? (
+                <img
+                  src="src/assets/images/icon-sun.svg"
+                  alt=""
+                  className="w-[25px] sm:w-[35px]"
+                />
+              ) : (
+                <img
+                  src="src/assets/images/icon-moon.svg"
+                  alt=""
+                  className="w-[25px] sm:w-[35px]"
+                />
+              )}
             </button>
           </div>
         </div>
       </div>
       <div className="w-full pt-10 px-2 flex flex-col justify-center items-center gap-3 sm:flex sm:flex-row  sm:justify-between">
         <div>
-          <h1 className="text-3xl sm:text-4xl text-white font-bold">
+          <h1
+            className={`text-3xl sm:text-4xl ${
+              darkmode ? "text-white" : "text-black"
+            }  font-bold`}
+          >
             Extensions List
           </h1>
         </div>
         <div className="flex gap-2 text-lg">
-          <button className="w-fit h-fit p-2 bg-[#1f2535] px-4 text-white rounded-2xl cursor-pointer hover:bg-red-400 hover:text-black  ">
+          <button
+            className={`w-fit h-fit p-2 ${
+              darkmode
+                ? "bg-[#1f2535] text-white hover:bg-red-400 hover:text-black"
+                : "bg-[#fcfdff] hover:bg-red-400 hover:text-white"
+            }  px-4  rounded-2xl cursor-pointer   `}
+          >
             All
           </button>
-          <button className="w-fit h-fit p-2 bg-[#1f2535] px-4 text-white  rounded-2xl cursor-pointer hover:bg-red-400 hover:text-black">
+          <button
+            className={`w-fit h-fit p-2 ${
+              darkmode
+                ? "bg-[#1f2535] text-white hover:bg-red-400 hover:text-black"
+                : "bg-[#fcfdff] hover:bg-red-400 hover:text-white"
+            }  px-4  rounded-2xl cursor-pointer   `}
+          >
             Active
           </button>
-          <button className="w-fit h-fit p-2 bg-[#1f2535] px-4 text-white  rounded-2xl cursor-pointer hover:bg-red-400 hover:text-black">
+          <button
+            className={`w-fit h-fit p-2 ${
+              darkmode
+                ? "bg-[#1f2535] text-white hover:bg-red-400 hover:text-black"
+                : "bg-[#fcfdff] hover:bg-red-400 hover:text-white"
+            }  px-4  rounded-2xl cursor-pointer   `}
+          >
             Inactive
           </button>
         </div>
--- fe1/tailwind.config.js
+++ fe1/tailwind.config.js
@@ -1,3 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // 👈 This tells Tailwind to scan your files
+  ],
   darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
