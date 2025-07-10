da20dd7fc6cafe1493968ac59dbf96fb33f67058
Sudipta <techtv70@gmail.com>
2025-07-10 17:33:34 +0530

    .

--- countriesinfo/src/App.tsx
+++ countriesinfo/src/App.tsx
@@ -1,34 +1,20 @@
 import { useState } from "react";
 import "./App.css";
-import Topbar from "./components/topbar";
-import { countries } from "./data/countries";
-import { IoMoonSharp } from "react-icons/io5";
+import { Topbar } from "./components/topbar";
 
 function App() {
   const [darkmode, setdarkmode] = useState(false);
+
+  function toggledark() {
+    setdarkmode(!darkmode);
+  }
   return (
     <div
       className={`w-full min-h-screen max-h-full ${
         darkmode ? "bg-[#202d36]" : "bg-[#fafafa]"
       }  `}
     >
-      <div
-        className={`w-full h-18 ${
-          darkmode
-            ? "bg-[#2b3743] text-gray-100"
-            : "bg-[#ffffff] text-slate-900"
-        }  flex justify-between items-center px-10 sm:px-19`}
-      >
-        <h1 className=" text-xl sm:text-2xl font-semibold">
-          Where in the world?
-        </h1>
-        <button
-          onClick={() => setdarkmode(!darkmode)}
-          className=" text-sm sm:text-lg font-medium flex items-center gap-2 cursor-pointer"
-        >
-          <IoMoonSharp size={17} /> <p className="pb-0.5">Dark Mode</p>
-        </button>
-      </div>
+      <Topbar toggledark={toggledark} darkmode={darkmode} />
     </div>
   );
 }
--- countriesinfo/src/components/topbar.tsx
+++ countriesinfo/src/components/topbar.tsx
@@ -1,14 +1,27 @@
-const Topbar = () => {
-  return (
-    <header className="w-full bg-[#2b3743] text-white shadow-md">
-      <div className="max-w-7xl mx-auto px-4 py-4 flex items-center justify-between">
-        <h1 className="text-lg sm:text-xl font-bold">🌍 Country Info</h1>
-        <button className="text-sm sm:text-base hover:underline focus:outline-none">
-          🌙 Dark Mode
-        </button>
-      </div>
-    </header>
-  );
-};
+import { IoMoonSharp } from "react-icons/io5";
 
-export default Topbar;
+export function Topbar({
+  darkmode,
+  toggledark,
+}: {
+  darkmode: boolean;
+  toggledark: () => void;
+}) {
+  return (
+    <div
+      className={`w-full h-18 ${
+        darkmode ? "bg-[#2b3743] text-gray-100" : "bg-[#ffffff] text-slate-900"
+      }  flex justify-between items-center px-10 sm:px-19`}
+    >
+      <h1 className=" text-xl sm:text-2xl font-semibold">
+        Where in the world?
+      </h1>
+      <button
+        onClick={() => toggledark()}
+        className=" text-sm sm:text-lg font-medium flex items-center gap-2 cursor-pointer"
+      >
+        <IoMoonSharp size={17} /> <p className="pb-0.5">Dark Mode</p>
+      </button>
+    </div>
+  );
+}
