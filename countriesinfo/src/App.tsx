2d02fcd71f7233d4b05072b837ee0eac97753b91
Sudipta <techtv70@gmail.com>
2025-07-10 17:03:21 +0530

    .

--- countriesinfo/src/App.tsx
+++ countriesinfo/src/App.tsx
@@ -1,11 +1,8 @@
 import "./App.css";
+import { countries } from "./data/countries";
 
 function App() {
-  return (
-    <>
-      <div>hello</div>
-    </>
-  );
+  return <div className="w-full min-h-screen max-h-full bg-[#202d36]"></div>;
 }
 
 export default App;
