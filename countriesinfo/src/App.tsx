6ee0f57126ed4fc04159671dd69fd16ebdc28b8c
Sudipta <techtv70@gmail.com>
2025-07-10 17:29:00 +0530

    .

--- countriesinfo/src/App.tsx
+++ countriesinfo/src/App.tsx
@@ -1,18 +1,31 @@
+import { useState } from "react";
 import "./App.css";
 import Topbar from "./components/topbar";
 import { countries } from "./data/countries";
 import { IoMoonSharp } from "react-icons/io5";
 
 function App() {
+  const [darkmode, setdarkmode] = useState(false);
   return (
-    <div className={`w-full min-h-screen max-h-full bg-[#202d36]`}>
+    <div
+      className={`w-full min-h-screen max-h-full ${
+        darkmode ? "bg-[#202d36]" : "bg-[#fafafa]"
+      }  `}
+    >
       <div
-        className={`w-full h-18 bg-[#2b3743] flex justify-between items-center px-10 sm:px-19`}
+        className={`w-full h-18 ${
+          darkmode
+            ? "bg-[#2b3743] text-gray-100"
+            : "bg-[#ffffff] text-slate-900"
+        }  flex justify-between items-center px-10 sm:px-19`}
       >
-        <h1 className="text-gray-100 text-xl sm:text-2xl font-semibold">
+        <h1 className=" text-xl sm:text-2xl font-semibold">
           Where in the world?
         </h1>
-        <button className="text-gray-100 text-sm sm:text-lg font-medium flex items-center gap-2 cursor-pointer">
+        <button
+          onClick={() => setdarkmode(!darkmode)}
+          className=" text-sm sm:text-lg font-medium flex items-center gap-2 cursor-pointer"
+        >
           <IoMoonSharp size={17} /> <p className="pb-0.5">Dark Mode</p>
         </button>
       </div>
