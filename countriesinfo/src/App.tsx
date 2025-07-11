6d128dffb653ee9a0dfcc76be36a65f90ed6ec65
Sudipta <techtv70@gmail.com>
2025-07-11 20:29:15 +0530

    .

--- countriesinfo/src/App.tsx
+++ countriesinfo/src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { Topbar } from "./components/topbar";
 import { Card } from "./components/countrycard";
 import { Input } from "./components/ui/input";
+import { BsSearch } from "react-icons/bs";
 
 function App() {
   const [darkmode, setdarkmode] = useState(false);
@@ -19,14 +20,17 @@ function App() {
       <Topbar toggledark={toggledark} darkmode={darkmode} />
       <div className="w-full pt-8 mx-20 px-13 ">
         <div className="w-[450px] ">
-          <Input
-            type="text"
-            name="country"
-            placeholder="Enter country name"
-            className={`py-6 border ${
-              darkmode ? "border-gray-700" : "border-gray-300"
-            } `}
-          />
+          <div className="w-full relative">
+            <BsSearch className="absolute top-1/2 left-4 -translate-y-1/2 " />
+            <Input
+              type="text"
+              name="country"
+              placeholder="Enter country name"
+              className={`py-6 pl-12 border ${
+                darkmode ? "border-gray-700" : "border-gray-300"
+              } `}
+            />
+          </div>
         </div>
         <div></div>
       </div>
