97e861e0c7b70cc38f33109cc421ac7cb8f08fb0
Sudipta <techtv70@gmail.com>
2025-07-15 20:17:59 +0530

    .

--- countriesinfo/src/pages/countrypage.tsx
+++ countriesinfo/src/pages/countrypage.tsx
@@ -1,6 +1,10 @@
 import { useParams } from "react-router-dom";
+import { countries } from "../data/countries";
+import { Topbar } from "../components/topbar";
 
 export function CountryPage() {
   const { name } = useParams();
-  return <div className="w-full h-screen">{name}</div>;
+  const country = countries.find((c) => c.name === name);
+
+  return <div className="w-full h-screen">{country?.name}</div>;
 }
