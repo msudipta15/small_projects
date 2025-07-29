88bf9833bb3786ed62cfa0c0e0cdd787c79a8da7
Sudipta <techtv70@gmail.com>
2025-07-30 00:09:05 +0530

    .

--- mycompany/app/page.tsx
+++ mycompany/app/page.tsx
@@ -1,5 +1,17 @@
 import Image from "next/image";
+import { GiBrain } from "react-icons/gi";
 
 export default function Home() {
-  return <div>Mondal Enterprise</div>;
+  return (
+    <div className="h-screen">
+      <div>
+        <div>
+          <h1 className="text-center">Mondal Enterprise</h1>
+        </div>
+        <div className="w-full flex justify-center items-center">
+          <GiBrain size={90} />
+        </div>
+      </div>
+    </div>
+  );
 }
