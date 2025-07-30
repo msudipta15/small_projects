369f84e4f8622fd6042b6a49738814f2ced3606d
Sudipta <techtv70@gmail.com>
2025-07-30 19:06:39 +0530

    .

--- mycompany/app/page.tsx
+++ mycompany/app/page.tsx
@@ -3,14 +3,13 @@ import { GiBrain } from "react-icons/gi";
 
 export default function Home() {
   return (
-    <div className="h-screen">
-      <div>
-        <div>
-          <h1 className="text-center">Mondal Enterprise</h1>
-        </div>
-        <div className="w-full flex justify-center items-center">
-          <GiBrain size={90} />
-        </div>
+    <div className="h-screen w-full">
+      <div className="p-12">
+        <h1 className="text-6xl p-2 font-bold">Mondal</h1>
+        <h1 className="text-6xl p-2 font-bold">Enterprise</h1>
+      </div>
+      <div className="text-center mt-10 p-10">
+        <h1 className="text-6xl">Coming Soon</h1>
       </div>
     </div>
   );
